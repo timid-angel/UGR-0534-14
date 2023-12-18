3c87a06ffe92c431b23355f3320d919c69ad2e47
timid-angel <natms24@gmail.com>
2023-12-18 12:55:00 +0300

    Adjusted navbar

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,35 +1,53 @@
 const nav = document.querySelector('.header__nav');
 const navUl = document.querySelector('.header__nav-ul');
+const navMobile = document.querySelector('.body__mobile');
+const navLi = document.querySelectorAll('.mobile__li');
 const button = document.querySelector('.header__button');
+const body = document.querySelector('body');
 let buttonActive = false;
+const defaultLayout = "grid-template-rows: min(6rem, 12vh) 1fr min(4rem, 8vh);";
+let bodyGridLayout = "grid-template-rows: min(6rem, 12vh) max(6rem, min(15vh, 135px)) 1fr min(4rem, 8vh);"
 
-// event listener and handler for the nav button
+if (document.title === "Home") {
+    bodyGridLayout = "grid-template-rows: min(6rem, 12vh) max(8rem, min(20vh, 180px)) 1fr min(4rem, 8vh);"
+}
+
+// event handler for the nav button
 function clickHandler() {
     if (!buttonActive) {
-        nav.setAttribute('style', 'display: static;');
-        navUl.classList.add('header__nav-ul--menu');
+        navMobile.hidden = false;
+        body.setAttribute('style', bodyGridLayout);
         buttonActive = true;
     } else {
-        nav.setAttribute('style', 'display: none;');
-        navUl.classList.remove('header__nav-ul--menu');
+        navMobile.hidden = true;
+        body.setAttribute('style', defaultLayout);
         buttonActive = false;
     }
 }
 
-button.addEventListener('click', clickHandler);
-
-// the setInterval method is used to test the design with WebDev tools
-setInterval(() => {
-    if (window.innerWidth < 450 && !buttonActive) {
-        nav.setAttribute("style", "display: none;");
-        button.setAttribute("style", "display: block;");
-    } else if (window.innerWidth < 450 && buttonActive) {
-        nav.setAttribute("style", "display: block;");
-        button.setAttribute("style", "display: block;");
+// resets the navbar settings
+function resetNavbar() {
+    if (window.innerWidth < 450) {
+        nav.hidden = true;
+        button.hidden = false;
     } else {
-        nav.setAttribute("style", "display: block;");
-        button.setAttribute("style", "display: none;");
-        navUl.classList.remove('header__nav-ul--menu');
-        buttonActive = false;
+        nav.hidden = false;
+        button.hidden = true;
     }
-}, 100);
+    navMobile.hidden = true;
+    buttonActive = false;
+    body.setAttribute('style', defaultLayout);
+}
+
+button.addEventListener('click', clickHandler);
+window.addEventListener('resize', resetNavbar);
+
+navLi.forEach(li => {
+    const innerAnchor = li.childNodes[0];
+    li.addEventListener('click', () => {
+        innerAnchor.click();
+    })
+})
+
+// check window size upon page load
+resetNavbar();
